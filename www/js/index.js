eb07efcdd233b997829ee8053ec72f146d53cab3
Franklin Ejike <Ejike.franklin@ymail.com>
2020-04-28 03:01:42 +0100

    Initial commit

---
+++ www/js/index.js
@@ -0,0 +1,15 @@
+var Team = {
+    name:"Team-031",
+    project:"Zero Hunger",
+    solution:"Unknown",
+    members:9,
+};
+welcome =()=>
+    {
+        console.log(Team.name);
+        console.log(Team.project);
+        console.log(Team.solution);
+        console.log(Team.members);
+    }
+welcome();
+//this are all placeholders, pls clear all.
