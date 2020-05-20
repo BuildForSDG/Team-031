489df8e5cf610fc203f5a180346c49d7f990ab09
Franklin Ejike <Ejike.franklin@ymail.com>
2020-05-20 22:44:36 +0100

    login, signupm, and other major screens

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -4,4 +4,37 @@ var Team = {
     solution:"Unknown",
     members:9,
 };
+closeInxdex=()=>
+{
+	document.getElementById("index_page").style.marginLeft="-100vw";
+}
+openInxdex=()=>
+{
+	document.getElementById("index_page").style.marginLeft="0vw";
+}
+
+closeSignin=()=>
+{
+	document.getElementById("sign_page").style.marginLeft="100vw";
+}
+openSignin=()=>
+{
+	document.getElementById("sign_page").style.marginLeft="0vw";
+}
+closeRegister=()=>
+{
+	document.getElementById("register_page").style.marginLeft="100vw";
+}
+openRegister=()=>
+{
+	document.getElementById("register_page").style.marginLeft="0vw";
+}
+closeTraders=()=>
+{
+	document.getElementById("select_trader").style.marginLeft="100vw";
+}
+openTraders=()=>
+{
+	document.getElementById("select_trader").style.marginLeft="0vw";
+}
 //this are all placeholders, pls clear all.
