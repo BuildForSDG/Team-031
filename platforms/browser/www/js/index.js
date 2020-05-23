818b23b81c75fbdc1ee795613ed947c673a14fc1
Franklin Ejike <Ejike.franklin@ymail.com>
2020-05-23 12:04:47 +0100

    mobile view completed

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -4,45 +4,144 @@ var Team = {
     solution:"Unknown",
     members:9,
 };
+nav=(x)=>
+{
+	//screen navigation controls.
+	switch(x)
+	{
+		case "buy":
+            document.getElementById("buy_nav").style.borderBottom ="solid 1vh #73d14b";
+            document.getElementById("sell_nav").style.borderBottom ="solid 0vh #73d14b";
+            document.getElementById("Mystore_nav").style.borderBottom ="solid 0vh #73d14b";
+            document.getElementById("more_nav").style.borderBottom ="solid 0vh #73d14b";
+            //change screen
+            document.getElementById("product_selection").style.display="flex";
+            document.getElementById("sell_section").style.display="none";
+            document.getElementById("my_store").style.display="none";
+            document.getElementById("more_selection").style.display="none";
+            break;
+        case "sell":
+
+            document.getElementById("buy_nav").style.borderBottom ="solid 0vh #73d14b";
+            document.getElementById("sell_nav").style.borderBottom ="solid 1vh #73d14b";
+            document.getElementById("Mystore_nav").style.borderBottom= "solid 0vh #73d14b";
+            document.getElementById("more_nav").style.borderBottom ="solid 0vh #73d14b";
+            //change screen
+            document.getElementById("product_selection").style.display="none";
+            document.getElementById("sell_section").style.display="flex";
+            document.getElementById("my_store").style.display="none";
+            document.getElementById("more_selection").style.display="none";
+            break;
+        case "Mystore":
+
+            document.getElementById("buy_nav").style.borderBottom = "solid 0vh #73d14b";
+            document.getElementById("sell_nav").style.borderBottom = "solid 0vh #73d14b";
+            document.getElementById("Mystore_nav").style.borderBottom = "solid 1vh #73d14b";
+            document.getElementById("more_nav").style.borderBottom = "solid 0vh #73d14b";
+            //change screen
+            document.getElementById("product_selection").style.display="none";
+            document.getElementById("sell_section").style.display="none";
+            document.getElementById("my_store").style.display="flex";
+            document.getElementById("more_selection").style.display="none";
+            break;
+        case "more":
+
+            document.getElementById("buy_nav").style.borderBottom = "solid 0vh #73d14b";
+            document.getElementById("sell_nav").style.borderBottom = "solid 0vh #73d14b";
+            document.getElementById("Mystore_nav").style.borderBottom = "solid 0vh #73d14b";
+            document.getElementById("more_nav").style.borderBottom = "solid 1vh #73d14b";
+            //change screen
+            document.getElementById("product_selection").style.display="none";
+            document.getElementById("sell_section").style.display="none";
+            document.getElementById("my_store").style.display="none";
+            document.getElementById("more_selection").style.display="flex";
+            break;
+	}
+}
 closeInxdex=()=>
 {
+	//closes index screen
 	document.getElementById("index_page").style.marginLeft="-100vw";
 }
 openInxdex=()=>
 {
+	//index screen, to select opition to buy or sell prodect.
 	document.getElementById("index_page").style.marginLeft="0vw";
+	nav("buy");
 }
-
 closeSignin=()=>
 {
+	//close login screen 
 	document.getElementById("sign_page").style.marginLeft="100vw";
 }
 openSignin=()=>
 {
+	//open login screen
 	document.getElementById("sign_page").style.marginLeft="0vw";
 }
 closeRegister=()=>
 {
+	//closes signup screen
 	document.getElementById("register_page").style.marginLeft="100vw";
 }
 openRegister=()=>
 {
+	//open signup screen
 	document.getElementById("register_page").style.marginLeft="0vw";
 }
 closeTraders=()=>
 {
+	//closes list of traders
 	document.getElementById("select_trader").style.marginLeft="100vw";
 }
 openTraders=()=>
 {
+	//opens list of traders for selected item 
 	document.getElementById("select_trader").style.marginLeft="0vw";
 }
 closeBooking=()=>
 {
+	//closes booking screen
 	document.getElementById("place_order").style.marginLeft="100vw";
 }
 openBooking=()=>
 {
+	//open booking screen
 	document.getElementById("place_order").style.marginLeft="0vw";
 }
-//this are all placeholders, pls clear all.
+loader=(x)=>
+{
+	//used to prevent clicks when loading data from server / api.
+	//x = 0 (show).
+	//x = 100 (hide).
+	//call the fucntion loader(x) when needed.
+	document.getElementById("loading_page").style.marginLeft=x+"vw";
+}
+increment=(x)=>
+{
+	//increase price
+	var a=document.getElementById(x).value;
+	//convert to integar
+	a=parseInt(a);
+	//return increment
+	document.getElementById(x).value=a+5;
+}
+decrement=(x)=>
+{
+	//increase price
+	var a=document.getElementById(x).value;
+	//convert to integar
+	a=parseInt(a);
+	//return decrement
+	if(a===0)
+	{
+
+	}
+	else
+	{
+		document.getElementById(x).value=a-5;
+	}
+	
+}
+
+//add new fuctions / features.
